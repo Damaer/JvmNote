c52c1f18318a0702dd1f63cf2aca85075b664026
Damaer <976170778@qq.com>
2021-02-09 03:25:30 +0800

    增加JVM的架构

---
+++ JvmProject/src/chapter01/StackStructTest.java
@@ -0,0 +1,10 @@
+package chapter01;
+
+public class StackStructTest {
+    public static void main(String[] args) {
+        // int i = 2+3;
+        int i = 2;
+        int j = 3;
+        int k = i + j;
+    }
+}
